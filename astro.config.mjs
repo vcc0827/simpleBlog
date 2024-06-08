c19b1341a99ecd921b54893ce5abbe68246c0f0c
vcc0827 <vcc0827@github.com>
2024-06-08 16:04:26 +0800

    finished!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vccdemo1.netlify.app"
+  site: "https://vccdemo1.netlify.app",
+  integrations: [preact()]
 });
